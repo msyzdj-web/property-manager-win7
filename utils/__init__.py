87acee4c23b01722c58966422802b2fe856c8f24
msyzdj-web <msyzdj-web@example.com>
2026-01-03 16:47:35 +0800

    chore: make utils a package (add __init__.py) to fix PyInstaller import

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,14 @@
+"""
+utils package initializer.
+This file makes the utils directory a Python package so PyInstaller and imports like
+`from utils.printer import ReceiptPrinter` work correctly after packaging.
+"""
+
+__all__ = [
+    "printer",
+    "excel_exporter",
+    "backup_manager",
+]
+
 # Utils package
 
